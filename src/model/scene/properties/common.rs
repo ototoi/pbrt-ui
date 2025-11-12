88a445ff6dd3d43f2b93f7518db2f72d8be9601c
Toru Matsuoka <tx.matsuoka@gmail.com>
2025-11-12 22:42:01 +0900

    Update Properties

--- src/model/scene/properties/common.rs
+++ src/model/scene/properties/common.rs
@@ -31,6 +31,7 @@ pub struct Properties {
     pub params: HashMap<String, Vec<PropertyEntry>>,
     pub keys: Vec<String>,
 }
+
 impl Properties {
     pub fn new(props: &[(String, PropertyEntry)]) -> Self {
         let mut params = HashMap::new();
@@ -54,4 +55,122 @@ impl Properties {
     pub fn get_types(&self) -> &Vec<String> {
         &self.keys
     }
+}
+
+fn parse_floats(value: &str) -> Vec<f32> {
+    let mut floats = Vec::new();
+    for s in value.split_whitespace() {
+        if s == "inf" {
+            floats.push(f32::INFINITY);
+        } else if s == "-inf" {
+            floats.push(f32::NEG_INFINITY);
+        } else if let Ok(f) = s.parse::<f32>() {
+            floats.push(f);
+        }
+    }
+    floats
+}
+
+fn parse_ints(value: &str) -> Vec<i32> {
+    let mut ints = Vec::new();
+    for s in value.split_whitespace() {
+        if let Ok(i) = s.parse::<i32>() {
+            ints.push(i);
+        }
+    }
+    ints
+}
+
+fn parse_strings(value: &str) -> Vec<String> {
+    let mut strings = Vec::new();
+    for s in value.split_whitespace() {
+        strings.push(s.to_string());
+    }
+    if strings.is_empty() {
+        strings.push("".to_string());
+    }
+    strings
+}
+
+fn parse_bools(value: &str) -> Vec<bool> {
+    let mut bools = Vec::new();
+    for s in value.split_whitespace() {
+        if let Ok(b) = s.parse::<bool>() {
+            bools.push(b);
+        }
+    }
+    bools
+}
+
+fn parse_range(key_type: &str, range: &str) -> Option<ValueRange> {
+    if range.is_empty() {
+        return None;
+    }
+    match key_type {
+        "float" => {
+            let mut range = range.split_whitespace();
+            let min = range.next().unwrap_or("0.0").parse::<f32>().unwrap_or(0.0);
+            let max = range.next().unwrap_or("1.0").parse::<f32>().unwrap_or(1.0);
+            Some(ValueRange::FloatRange(min, max))
+        }
+        "integer" => {
+            let mut range = range.split_whitespace();
+            let min = range.next().unwrap_or("0").parse::<i32>().unwrap_or(0);
+            let max = range.next().unwrap_or("1").parse::<i32>().unwrap_or(1);
+            Some(ValueRange::IntRange(min, max))
+        }
+        _ => None,
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub enum PropetyParseKey {
+    Name,
+    KeyType,
+    KeyName,
+    DefaultValue,
+    ValueRange,
+}
+
+pub fn parse_property_entry(param: &HashMap<PropetyParseKey, String>) -> (String, PropertyEntry) {
+    let name = param
+        .get(&PropetyParseKey::Name)
+        .unwrap_or(&"".to_string())
+        .clone();
+    let key_type = param
+        .get(&PropetyParseKey::KeyType)
+        .unwrap_or(&"".to_string())
+        .clone();
+    let key_name = param
+        .get(&PropetyParseKey::KeyName)
+        .unwrap_or(&"".to_string())
+        .clone();
+    let default_value = param
+        .get(&PropetyParseKey::DefaultValue)
+        .unwrap_or(&"".to_string())
+        .clone();
+    let value_range = param
+        .get(&PropetyParseKey::ValueRange)
+        .unwrap_or(&"".to_string())
+        .clone();
+    let default_value = match key_type.as_str() {
+        "point" | "color" | "vector" | "normal" | "float" => {
+            Property::from(parse_floats(&default_value))
+        }
+        "integer" => Property::from(parse_ints(&default_value)),
+        "string" | "spectrum" | "texture" => Property::from(parse_strings(&default_value)),
+        "bool" => Property::from(parse_bools(&default_value)),
+        _ => panic!("Unknown parameter type: {}", key_type),
+    };
+    let value_range = parse_range(&key_type, &value_range);
+    return (
+        name.to_string(),
+        PropertyEntry {
+            key_type,
+            key_name,
+            default_value,
+            value_range,
+            ..Default::default()
+        },
+    );
 }
--- src/model/scene/properties/mapping.rs
+++ src/model/scene/properties/mapping.rs
@@ -1,7 +1,6 @@
 use super::common::*;
-use super::value_range::ValueRange;
-use crate::model::base::*;
 use std::cell::LazyCell;
+use std::collections::HashMap;
 
 const PARAMETERS: [(&str, &str, &str, &str, &str); 8] = [
     ("uv", "float", "uscale", "1.0", "0.0 100.0"),
@@ -14,100 +13,23 @@ const PARAMETERS: [(&str, &str, &str, &str, &str); 8] = [
     ("planar", "float", "vdelta", "0.0", "0.0 100.0"),
 ];
 
-fn parse_floats(value: &str) -> Vec<f32> {
-    let mut floats = Vec::new();
-    for s in value.split_whitespace() {
-        if let Ok(f) = s.parse::<f32>() {
-            floats.push(f);
-        }
-    }
-    floats
-}
-
-fn parse_ints(value: &str) -> Vec<i32> {
-    let mut ints = Vec::new();
-    for s in value.split_whitespace() {
-        if let Ok(i) = s.parse::<i32>() {
-            ints.push(i);
-        }
-    }
-    ints
-}
-
-fn parse_strings(value: &str) -> Vec<String> {
-    let mut strings = Vec::new();
-    for s in value.split_whitespace() {
-        strings.push(s.to_string());
-    }
-    if strings.is_empty() {
-        strings.push("".to_string());
-    }
-    strings
-}
-
-fn parse_bools(value: &str) -> Vec<bool> {
-    let mut bools = Vec::new();
-    for s in value.split_whitespace() {
-        if let Ok(b) = s.parse::<bool>() {
-            bools.push(b);
-        }
-    }
-    bools
-}
-
-fn parse_range(key_type: &str, range: &str) -> Option<ValueRange> {
-    if range.is_empty() {
-        return None;
-    }
-    match key_type {
-        "float" => {
-            let mut range = range.split_whitespace();
-            let min = range.next().unwrap_or("0.0").parse::<f32>().unwrap_or(0.0);
-            let max = range.next().unwrap_or("1.0").parse::<f32>().unwrap_or(1.0);
-            Some(ValueRange::FloatRange(min, max))
-        }
-        "integer" => {
-            let mut range = range.split_whitespace();
-            let min = range.next().unwrap_or("0").parse::<i32>().unwrap_or(0);
-            let max = range.next().unwrap_or("1").parse::<i32>().unwrap_or(1);
-            Some(ValueRange::IntRange(min, max))
-        }
-        _ => None,
-    }
-}
-
-fn parse_parameter(param: (&str, &str, &str, &str, &str)) -> (String, PropertyEntry) {
-    let (name, key_type, key_name, value, range) = param;
-    let key_type = key_type.to_string();
-    let key_name = key_name.to_string();
-    let value = match key_type.as_str() {
-        "point" | "vector" | "normal" | "color" | "float" => Property::from(parse_floats(value)),
-        "integer" => Property::from(parse_ints(value)),
-        "string" | "spectrum" | "texture" => Property::from(parse_strings(value)),
-        "bool" => Property::from(parse_bools(value)),
-        _ => panic!("Unknown parameter type"),
-    };
-    let range = parse_range(&key_type, range);
-
-    (
-        name.to_string(),
-        PropertyEntry {
-            key_type,
-            key_name,
-            default_value: value,
-            value_range: range,
-            ..Default::default()
-        },
-    )
-}
-
 #[derive(Debug, Clone)]
 pub struct MappingProperties;
 
 impl MappingProperties {
     fn new() -> Properties {
-        let props: Vec<(String, PropertyEntry)> =
-            PARAMETERS.iter().map(|p| parse_parameter(*p)).collect();
+        let props: Vec<(String, PropertyEntry)> = PARAMETERS
+            .iter()
+            .map(|(name, key_type, key_name, default_value, value_range)| {
+                let mut param = HashMap::new();
+                param.insert(PropetyParseKey::Name, name.to_string());
+                param.insert(PropetyParseKey::KeyType, key_type.to_string());
+                param.insert(PropetyParseKey::KeyName, key_name.to_string());
+                param.insert(PropetyParseKey::DefaultValue, default_value.to_string());
+                param.insert(PropetyParseKey::ValueRange, value_range.to_string());
+                return parse_property_entry(&param);
+            })
+            .collect();
         Properties::new(&props)
     }
     pub fn get_instance() -> LazyCell<Properties> {
